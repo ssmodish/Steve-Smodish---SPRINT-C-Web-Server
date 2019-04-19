54ddc890a11356d4d22c3b137b6b8ebbc48426c7
Steve Smodish <smodish@hotmail.com>
2019-04-19 13:35:08 -0400

    Sending request, now trying to recv

--- src/client.c
+++ src/client.c
@@ -45,9 +45,29 @@ urlinfo_t *parse_url(char *url)
     6. Overwrite the colon with a '\0' so that we are just left with the hostname.
   */
 
+
   ///////////////////
   // IMPLEMENT ME! //
   ///////////////////
+  printf("parse_url 1.0: hostname - %s\n", hostname);
+  char *divider = strchr(hostname, '/');
+  if(divider){
+    path = (divider + 1);
+    printf("parse_url 1.1: path - %s\n", path);
+    hostname = strtok(hostname, "/");
+  }
+  printf("parse_url 1.2: hostname - %s\n", hostname);
+  divider = strchr(hostname, ':');
+  if(divider){
+    port = (divider + 1);
+    printf("parse_url 1.3: port - %s\n", port);
+    hostname = strtok(hostname, ":");
+  }
+  printf("parse_url 1.4: hostname - %s\n", hostname);
+
+  urlinfo->hostname = hostname;
+  urlinfo->port = port;
+  urlinfo->path = path;
 
   return urlinfo;
 }
@@ -71,8 +91,19 @@ int send_request(int fd, char *hostname, char *port, char *path)
   ///////////////////
   // IMPLEMENT ME! //
   ///////////////////
+  printf("send_request 1.0\n");
 
-  return 0;
+  sprintf(request,  "GET /%s HTTP/1.1\n"
+                    "Host: %s:%s\n"
+                    "Connection: close", path, hostname, port);
+
+  printf("send_request 1.1 REQUEST\n%s\n", request);
+
+  fd = get_socket(hostname, port);
+  printf("send_request 1.2 SOCKET: %d\n", fd);
+
+  return send(fd, request, strlen(request), 0);
+
 }
 
 int main(int argc, char *argv[])
@@ -96,6 +127,24 @@ int main(int argc, char *argv[])
   ///////////////////
   // IMPLEMENT ME! //
   ///////////////////
+  printf("main 1.0\n");
+  urlinfo_t *urlinfo = parse_url(argv[1]);
+  printf("main 1.1\n");
+  printf("urlinfo->hostname: %s\n", urlinfo->hostname);
+  printf("urlinfo->port: %s\n", urlinfo->port);
+  sockfd = get_socket(urlinfo->hostname, urlinfo->port);
+  printf("main 1.2\n");
+  send_request(sockfd, urlinfo->hostname, urlinfo->port, urlinfo->path);
+  printf("main 1.3\n");
+  recv(sockfd, buf, BUFSIZE, 0);
+  printf("main 1.4\n");
+  while(buf > 0){
+    printf("main 2.0\n");
+    printf("%s", buf);
+    printf("main 2.1\n");
+    recv(sockfd, buf, BUFSIZE, 0);
+  }
+  printf("main 1.5\n");
 
   return 0;
 }
